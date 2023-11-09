96a1e8ce89b99e793bf0c75be67ea3fecc035fe5
Alina <alinaspicot@gmail.com>
2023-11-09 10:16:12 +0100

    Ajout au graphique

--- script.js
+++ script.js
@@ -2,5 +2,13 @@
 
 //afficher quelques données du fichier json test
 fetch('data.json').then(function (response){
-    response.json().then(function () {};
-)};
+    response.json().then(function () {
+
+        d3.select("#graphique")
+    .selectAll(".noms")
+    .data(data)
+    .join("div")
+    .attr("class","noms")
+    .text(`state : ${state}`);
+    });
+});
