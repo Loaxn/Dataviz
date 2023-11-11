11d20ccd1009d22ffd22727afc8f54aaf1b166ac
Loaxn <chalachloana@gmail.com>
2023-11-11 20:15:05 +0100

    graphe test

--- script.js
+++ script.js
@@ -1,14 +1,70 @@
 // code de Alina
 
 //afficher quelques données du fichier json test
-fetch('data.json').then(function (response){
-    response.json().then(function () {
+// fetch('data.json').then(function (response){
+//     response.json().then(function () {
 
-        d3.select("#graphique")
-    .selectAll(".noms")
-    .data(data)
-    .join("div")
-    .attr("class","noms")
-    .text(`state : ${state}`);
-    });
+//     d3.select("#graphique")
+//     .selectAll(".noms")
+//     .data(data)
+//     .join("div")
+//     .attr("class","noms")
+//     .text(`state : ${state}`);
+//     });
+// });
+
+// CECI EST UN EXEMPLE reste à mettre les vrais données
+document.addEventListener('DOMContentLoaded', function () {
+    var donnees = [
+        { "nom": "Heroine", "valeur": 20 },
+        { "nom": "Cocaine", "valeur": 50 },
+        { "nom": "Methadone", "valeur": 30 },
+        { "nom": "Methadone", "valeur": 10 }
+        // Ajoutez les autres données ici
+    ];
+
+    // Configuration du graphique
+    var largeur = 750;
+    var hauteur = 200;
+
+    // Création de l'échelle pour l'axe des x
+    var echelleX = d3.scaleBand()
+        .domain(donnees.map(function (d) { return d.nom; }))
+        .range([0, largeur])
+        .padding(0.1);
+
+    // Création de l'échelle pour l'axe des y
+    var echelleY = d3.scaleLinear()
+        .domain([0, d3.max(donnees, function (d) { return d.valeur; })])
+        .range([hauteur, 0]);
+
+    // Création du conteneur SVG
+    var svg = d3.select("#graphique")
+        .append("svg")
+        .attr("width", largeur)
+        .attr("height", hauteur);
+
+    // Création de la ligne
+    var ligne = d3.line()
+        .x(function (d) { return echelleX(d.nom) + echelleX.bandwidth() / 2; }) // Centre de la bande
+        .y(function (d) { return echelleY(d.valeur); });
+
+    // Ajout de la ligne au graphique
+    svg.append("path")
+        .datum(donnees)
+        .attr("d", ligne)
+        .attr("fill", "none")
+        .attr("stroke", "red");
+
+    // Ajout des points de données
+    svg.selectAll(".point")
+        .data(donnees)
+        .enter().append("circle")
+        .attr("class", "point")
+        .attr("cx", function (d) { return echelleX(d.nom) + echelleX.bandwidth() / 2; })
+        .attr("cy", function (d) { return echelleY(d.valeur); })
+        .attr("r", 4) // rayon du cercle
+
+   
 });
+ 
