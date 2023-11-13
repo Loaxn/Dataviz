ea6edf9ad983d7c01be41c6444ff33c62e2b6d35
Loaxn <chalachloana@gmail.com>
2023-11-13 18:10:52 +0100

    première tentative de dessiner le graphe
    j'ai regrouper toutes les informations utiles  dans un tableaux et ceci en fonction de l'etat choisis , reste à dessiner le graphe

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-document.querySelectorAll("path").forEach(element => {
-    element.addEventListener('click', function() {
-        const CRITERIA_STATE = this.id;
+// tous les éléments de type "path" et "polygon" et ajoutez un écouteur d'événements pour le clic
+document.querySelectorAll("path,polygon").forEach(element => {
+element.addEventListener('click', function() {
+// on récupère l'identifiant de l'État à partir de l'élément cliqué
+const CRITERIA_STATE = this.id;
+// Effectuez une requête pour récupérer les données du fichier JSON
+fetch('data2.json')
+    .then((response) => response.json())
+    .then((dataFetched) => {
+        // on definit la plage d'années de 2015 à 2023
+        const yearRange = Array.from({ length: 9 }, (_, index) => (2015 + index).toString());
+        // on definit l'indicateur comme 'Methadone (T40.3)'
+        const CRITERIA_INDICATOR = 'Methadone (T40.3)'
+        // stockage de la somme des valeurs de données pour chaque année
+        const sumValuesByYear = {};
+        //  filtrez en fonction des critères
+        dataFetched.forEach(d => {
+        if (
+            yearRange.includes(d.Year) &&
+            d && d["State Name"] === CRITERIA_STATE &&
+            d.Indicator === CRITERIA_INDICATOR
+            ) {
+            // Initialisez la somme pour l'année si elle n'existe pas encore
+            if (!sumValuesByYear[d.Year]) {
+                sumValuesByYear[d.Year] = 0;
+            }
 
-        fetch('data2.json')
-            .then((response) => response.json())
-            .then((dataFetched) => {
-                const CRITERIA_YEAR = "2016";
-                const CRITERIA_INDICATOR = 'Heroin (T40.1)';
+// on ajoute la valeur de données convertie en nombre à la somme de l'année correspondante
+const numericValue = parseFloat(d["Data Value"].replace(',', ''));
 
-                const dataReduced = dataFetched.filter(d => (
-                    d.Year === CRITERIA_YEAR &&
-                    d && d["State Name"] === CRITERIA_STATE &&
-                    d.Indicator === CRITERIA_INDICATOR
-                ));
-
-                const dataValues = dataReduced.map(d => (
-                    d && d["Data Value"]
-                ));
-                console.log(dataValues);
-                // Now you can use dataValues to display or process the data as needed
+// on verifie si la conversion a réussi (remplacez NaN par 0)
+ if (!isNaN(numericValue)) {
+     sumValuesByYear[d.Year] += numericValue;
+ }
+}
+});
+console.log(sumValuesByYear);
             });
     });
 });
+
+//Dessiner le graphe en utilisant la somme 
+
+
