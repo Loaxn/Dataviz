8167768fb945c7fefab464072deb98b22a0f0d53
Alina <alinaspicot@gmail.com>
2023-11-18 15:51:44 +0100

    graphqiue
    -Titre pour la Graphique
    -Amélioration du front
    -J'ai supprimé les opiodes parce que c'est plus logique de ne pas les mettre

--- script.js
+++ script.js
@@ -1,145 +1,138 @@
-//on selectionne les paths et les polygon de la carte
+//Afficher le graphique de Alaska et heroine par défaut
+CRITERIA_STATE = "Alaska";
+fetch('data2.json')
+    .then((response) => response.json())
+    .then((dataFetched) => {
+ // on définis la plage d'années de 2015 à 2023
+ const yearRange = Array.from({ length: 9 }, (_, index) => (2015 + index).toString());
 
-// document.querySelectorAll("path, polygon").forEach(element => {
-// element.addEventListener('click', function () {
-// //on récupère les etats choisis
-//     const CRITERIA_STATE = this.id;
-// fetch('data2.json')
-//     .then((response) => response.json())
-//     .then((dataFetched) => {
-//  // on définis la plage d'années de 2015 à 2023
-//  const yearRange = Array.from({ length: 9 }, (_, index) => (2015 + index).toString());
+ // on definit l'indicateur comme Methadone ou heroine
+ const CRITERIA_INDICATOR = 'Heroin (T40.1)';
 
-//  // on definit l'indicateur comme Methadone ou heroine
-//  const CRITERIA_INDICATOR = 'Heroin (T40.1)';
+ // on stocke la somme dans un tableau
+ const sumValuesByYear = Array(yearRange.length).fill(0); 
 
-//  // on stocke la somme dans un tableau
-//  const sumValuesByYear = Array(yearRange.length).fill(0); 
-
-//  // on filtre en fonction des critères
-//  dataFetched.forEach(d => {
-//      if (
-//          yearRange.includes(d.Year) &&
-//          d && d["State Name"] === CRITERIA_STATE &&
-//          d.Indicator === CRITERIA_INDICATOR
-//      ) {
-//          // Convertissez la valeur de données en nombre
-//          const numericValue = parseFloat(d["Data Value"].replace(',', ''));
-//          // remplace les NaN en 0 
-//          const yearIndex = yearRange.indexOf(d.Year);
-//          if (!isNaN(numericValue)) {
-//              sumValuesByYear[yearIndex] += numericValue;
-//              console.log(sumValuesByYear)
-//          }
-//      }
-//  });
+ // on filtre en fonction des critères
+ dataFetched.forEach(d => {
+     if (
+         yearRange.includes(d.Year) &&
+         d && d["State Name"] === CRITERIA_STATE &&
+         d.Indicator === CRITERIA_INDICATOR
+     ) {
+         // Convertissez la valeur de données en nombre
+         const numericValue = parseFloat(d["Data Value"].replace(',', ''));
+         // remplace les NaN en 0 
+         const yearIndex = yearRange.indexOf(d.Year);
+         if (!isNaN(numericValue)) {
+             sumValuesByYear[yearIndex] += numericValue;
+            //  console.log(sumValuesByYear)
+         }
+     }
+ });
 
 
-//     const dataPoints = yearRange.map((year, index) => ({
-//         x: parseInt(year, 10), // Utilisez parseInt pour obtenir un nombre entier mais ça marche pas 
-//         y: sumValuesByYear[index]
-//     }));
+    const dataPoints = yearRange.map((year, index) => ({
+        x: parseInt(year, 10), // Utilisez parseInt pour obtenir un nombre entier mais ça marche pas 
+        y: sumValuesByYear[index]
+    }));
 
-//     // Utilisez D3.js pour créer un graphique en ligne
-//     const svg = d3.select('.graphique');
-//     const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-//     const width = +svg.attr('width') - margin.left - margin.right;
-//     const height = +svg.attr('height') - margin.top - margin.bottom;
+    // Utilisez D3.js pour créer un graphique en ligne
+    const svg = d3.select('.graphique');
+    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    const width = +svg.attr('width') - margin.left - margin.right;
+    const height = +svg.attr('height') - margin.top - margin.bottom;
 
-//     const x = d3.scaleLinear()
-//         .range([0, width])
-//         .domain(d3.extent(dataPoints, d => d.x));
+    const x = d3.scaleLinear()
+        .range([0, width])
+        .domain(d3.extent(dataPoints, d => d.x));
 
-//     const y = d3.scaleLinear()
-//         .range([height, 0])
-//         .domain([0, d3.max(dataPoints, d => d.y)]);
+    const y = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, d3.max(dataPoints, d => d.y)]);
 
-//     const line = d3.line()
-//         .x(d => x(d.x))
-//         .y(d => y(d.y));
+    const line = d3.line()
+        .x(d => x(d.x))
+        .y(d => y(d.y));
 
-//     // Effacez le contenu existant du groupe
-//         svg.select(".graphGroup").remove();
+    // Effacez le contenu existant du groupe
+        svg.select(".graphGroup").remove();
 
 
-//     // Ajoutez un groupe pour le tracé du graphe
-//     const graphGroup = svg.append('g')
-//         .attr('transform', `translate(${margin.left},${margin.top})`)
-//         .attr('class',"graphGroup");
+    // Ajoutez un groupe pour le tracé du graphe
+    const graphGroup = svg.append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`)
+        .attr('class',"graphGroup");
    
 
 
-//     // Ajoutez la ligne du graphique avec le dégradé
-//     const linePath = graphGroup.append('path')
-//         .data([dataPoints])
-//         .attr('class', 'line')
-//         .attr('d', line)
-//         .style('stroke', 'url(#line-gradient)') // Utilisez le dégradé
-//         .style('stroke-width', 3) // Ajustez l'épaisseur du trait
-//         .attr('fill', 'none');
+    // Ajoutez la ligne du graphique avec le dégradé
+    const linePath = graphGroup.append('path')
+        .data([dataPoints])
+        .attr('class', 'line')
+        .attr('d', line)
+        .style('stroke', 'url(#line-gradient)') // Utilisez le dégradé
+        .style('stroke-width', 3) // Ajustez l'épaisseur du trait
+        .attr('fill', 'none');
 
-//     // Création et Ajout du dégradé au SVG
-//     svg.append("defs").append("linearGradient")
-//         .attr("id", "line-gradient")
-//         .attr("gradientUnits", "userSpaceOnUse")
-//         .attr("x1", 0).attr("y1", y(0))
-//         .attr("x2", 0).attr("y2", y(d3.max(dataPoints, d => d.y)))
-//         .selectAll("stop")
-//         .data([
-//             { offset: "0%", color: "#E42A2A" },
-//             { offset: "50%", color: "#DA5B58" },
-//             { offset: "100%", color: "#ED8240" }
-//         ])
-//         .enter().append("stop")
-//         .attr("offset", d => d.offset)
-//         .attr("stop-color", d => d.color);
+    // Création et Ajout du dégradé au SVG
+    svg.append("defs").append("linearGradient")
+        .attr("id", "line-gradient")
+        .attr("gradientUnits", "userSpaceOnUse")
+        .attr("x1", 0).attr("y1", y(0))
+        .attr("x2", 0).attr("y2", y(d3.max(dataPoints, d => d.y)))
+        .selectAll("stop")
+        .data([
+            { offset: "0%", color: "#E42A2A" },
+            { offset: "50%", color: "#DA5B58" },
+            { offset: "100%", color: "#ED8240" }
+        ])
+        .enter().append("stop")
+        .attr("offset", d => d.offset)
+        .attr("stop-color", d => d.color);
 
-//     // pour obtenir la longueur totale du chemin
-//     const totalLength = linePath.node().getTotalLength();
+    // pour obtenir la longueur totale du chemin
+    const totalLength = linePath.node().getTotalLength();
 
-//     // Ajout de l'animation du tracé
-//     linePath
-//         .attr('stroke-dasharray', `${totalLength} ${totalLength}`)
-//         .attr('stroke-dashoffset', totalLength)
-//         .transition()
-//         .duration(1500) // Durée de l'animation en millisecondes
-//         .ease(d3.easeLinear)
-//         .attr('stroke-dashoffset', 0);
+    // Ajout de l'animation du tracé
+    linePath
+        .attr('stroke-dasharray', `${totalLength} ${totalLength}`)
+        .attr('stroke-dashoffset', totalLength)
+        .transition()
+        .duration(1500) // Durée de l'animation en millisecondes
+        .ease(d3.easeLinear)
+        .attr('stroke-dashoffset', 0);
 
-//     // Ajout des cercles pour chaque point de données
-//     graphGroup.selectAll('circle')
-//         .data(dataPoints)
-//         .enter().append('circle')
-//         .attr('cx', d => x(d.x))
-//         .attr('cy', d => y(d.y))
-//         .attr('r', 4) // Rayon du cercle
-//         .attr('fill', 'white'); // Couleur du cercl
+    // Ajout des cercles pour chaque point de données
+    graphGroup.selectAll('circle')
+        .data(dataPoints)
+        .enter().append('circle')
+        .attr('cx', d => x(d.x))
+        .attr('cy', d => y(d.y))
+        .attr('r', 4) // Rayon du cercle
+        .attr('fill', 'white'); // Couleur du cercl
 
-//     // Ajout des axes x et y
-//     graphGroup.append('g')
-//         .attr('transform', `translate(0, ${height})`)
-//         .call(d3.axisBottom(x))
-//         .style('fill','white')
-//         .selectAll('text')
-//         .style('fill', 'white') 
-//         .style('font-size', '1rem'); 
+    // Ajout des axes x et y
+    graphGroup.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(x))
+        .style('fill','white')
+        .selectAll('text')
+        .style('fill', 'white') 
+        .style('font-size', '1rem'); 
         
 
-//     graphGroup.append('g')
-//         .call(d3.axisLeft(y))
-//         .attr('fill', 'white')
-//         .selectAll('text')
-//         .style('fill', 'white') 
-//         .style('font-size', '1rem'); 
+    graphGroup.append('g')
+        .call(d3.axisLeft(y))
+        .attr('fill', 'white')
+        .selectAll('text')
+        .style('fill', 'white') 
+        .style('font-size', '1rem'); 
 
+        d3.select(".graphiqueboutons p")
+        .text("Heroine in " + CRITERIA_STATE)
 
     
-// });
-
-//         // au moment du clique sur la carte on descend vers le graphe correspondant
-//         document.getElementById('graphique').scrollIntoView({ behavior: 'smooth', block: 'start' });
-//     });
-// });
+});
 
 function tracergraph () {//fonction qui trace le graphique
 //on récupère les etats choisis
@@ -272,6 +265,8 @@ fetch('data2.json')
         .style('fill', 'white') 
         .style('font-size', '1rem'); 
 
+        d3.select(".graphiqueboutons p")
+        .text("Heroine in " + CRITERIA_STATE)
 
     
 });
@@ -411,6 +406,11 @@ function tracedrogue () {//fonction qui trace le graphique
 
             d3.select('.info h3')
             .text(this.value);
+
+            d3.select(".graphiqueboutons p")
+        .text(this.value + " in " + CRITERIA_STATE)
+            
+
     });
     
             // au moment du clique sur la carte on descend vers le graphe correspondant
